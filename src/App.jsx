e98a0a5394619bfb9c2a90d16e9bb3cb9b88b77b
Soyamprabha <mallick.soyam@gmail.com>
2023-08-21 18:37:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Upload from "./pages/upload";
+import Drunk from "./pages/drunk";
+import Sober from "./pages/sober";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/upload" element={<Upload />} />
+        <Route exact path="/upload/drunk" element={<Drunk />} />
+        <Route exact path="/upload/sober" element={<Sober />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
